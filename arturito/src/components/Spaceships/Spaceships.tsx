5db34143fe4a5f8b5c1f4e377fc4af78dd837a9e
Chwaksen <agustinmandarini47@gmail.com>
2023-09-22 21:22:23 -0300

    FINISHED PRUEBA 1

--- arturito/src/components/Spaceships/Spaceships.tsx
+++ arturito/src/components/Spaceships/Spaceships.tsx
@@ -1,5 +1,7 @@
 // PRUEBA 1
 
+// FINISHED!!!!
+
 // const Button = ({ text, href }: { text: string; href: string }) => (
 //     <a className="mx-auto" href={href} target="_blank" rel="noreferrer">
 //       <div className="inline-block px-4 py-2 rounded-lg text-white bg-blue-500">
